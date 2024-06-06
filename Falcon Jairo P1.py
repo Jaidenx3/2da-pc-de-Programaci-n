1140c589c2c2089a0693d3db29e237a01a3fcadb
Jaidenx3 <jairo.falcon@unmsm.edu.pe>
2024-06-06 11:55:47 -0500

    Update Falcon Jairo P1.py

--- Falcon Jairo P1.py
+++ Falcon Jairo P1.py
@@ -1,3 +1,4 @@
+#https://github.com/Jaidenx3/2da-pc-de-Programaci-n
 enfermedades = ["gripe","indigestión","catarro","resfriado común","mononucleosis","artritis","gastritis","apendicitis","fibromialgia","epilepsia"]
 síntomas = ["fiebre","tos","dolor de garganta","nauseas","dolor abdominal","dolor en las articulaciones","sudoración excesiva","escalofríos","mareos","dolor de espalda"]
 Sintomatologías = [[0,0,0,0,0,0,0,0,0,1],
@@ -9,33 +10,34 @@ Sintomatologías = [[0,0,0,0,0,0,0,0,0,1],
                    [0,0,0,1,1,1,1,1,1,1],
                    [0,0,1,1,1,1,1,1,1,1],
                    [0,1,1,1,1,1,1,1,1,1],
-                   [1,1,1,1,1,1,1,1,1,1],
-                   [1,1,1,1,1,0,1,1,1,1],]
-paciente = []
+                   [1,1,1,1,1,1,1,1,1,1]]
 
-print("Pon 1 si presenta o 0 sino presenta:")
+def Enfermedad():
+    paciente = []
 
-def Diagnóstico():
-    Valor = False 
+    print("Pon 1 si presenta o 0 sino presenta:")
+    
     for i in range(10):
         while True:
             try:
                 X = int(input(f"Ingrese si padece {síntomas[i]}: "))
-                if X not in [0,1]:
+                if X not in [0, 1]:
                     print("Error: opción no válida.")
                     continue
                 break
             except ValueError:
                 print("Error: opción no válida. Ingrese solo 0 o 1.")
-
+        
         paciente.append(X)
-
-    for i in range(10):
-        if paciente == Sintomatologías[i]:
+    
+    encontrado = False
+    for i, sintomas in enumerate(Sintomatologías):
+        if paciente == sintomas:
             print(f"Usted padece de {enfermedades[i]}")
-            Valor = True
-
-    if not Valor:
+            encontrado = True
+            break
+    
+    if not encontrado:
         print("No existen coincidencias.")
 
-Diagnóstico()
+Enfermedad()
